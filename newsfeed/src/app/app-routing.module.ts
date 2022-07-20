3a55027dc97298a885e2b55d110ea9e678fb9ee1
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-07-21 01:52:33 +0300

    create user feed routing

--- newsfeed/src/app/app-routing.module.ts
+++ newsfeed/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllfeedsComponent } from './components/allfeeds/allfeeds.component';
+import { UserfeedsComponent } from './components/userfeeds/userfeeds.component';
 
 const routes: Routes = [
-  {path: '', component: AllfeedsComponent}
+  {path: '', component: AllfeedsComponent},
+  {path: 'userFeed', component: UserfeedsComponent}
 ];
 
 @NgModule({
