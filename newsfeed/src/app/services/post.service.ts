895f02be1e8c002ca00b90713b320c31a58a8b90
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-07-21 21:23:51 +0300

    create post service

---
+++ newsfeed/src/app/services/post.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Post } from '../models/post';
+import { Observable, catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  // BASE URL
+  baseurl = 'http://localhost:3000';  
+
+ 
+
+ constructor(private http: HttpClient) { }
+ 
+    // GET POSTS
+  GetAllPosts(): Observable<Post[]>{
+   return this.http.get<Post[]>(this.baseurl + '/posts')
+ }
+
+  
+}
