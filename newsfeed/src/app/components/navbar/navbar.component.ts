f02d124297e7c7d925b2834f7e56c63b69c679b6
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-07-21 13:47:57 +0300

    create users for loop and add styling

--- newsfeed/src/app/components/navbar/navbar.component.ts
+++ newsfeed/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent  implements OnInit {
+  UsersList: any = [];
 
-  constructor() { }
+
+
+  constructor(
+    public userService: UserService
+  ) { }
+
+  
 
   ngOnInit(): void {
+    this.userService.GetAllUsers().subscribe(res => this.UsersList = res)
   }
 
 }
