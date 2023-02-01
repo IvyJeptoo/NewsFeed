45f6540acc04e2c4381e8d8f47df215eaf8ac00b
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2023-02-01 14:49:43 +0300

    create module routing path

--- newsfeed/src/app/app-routing.module.ts
+++ newsfeed/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AllfeedsComponent } from './components/allfeeds/allfeeds.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AllfeedsComponent } from './feeds/allfeeds/allfeeds.component';
+import { NavbarComponent } from './feeds/navbar/navbar.component';
 
 const routes: Routes = [
   {path: '', component: AllfeedsComponent},
   {path: 'userFeed', component: NavbarComponent}
 ];
 
+const appRoutes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import ('./feeds/feeds.module').then(M => M.FeedsModule)
+  }
+]
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
