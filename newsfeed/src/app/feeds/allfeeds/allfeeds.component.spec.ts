4b5562c948e74f1e247916578e5103217788c7f6
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2023-02-01 14:37:36 +0300

    create the feeds module and transferred components

---
+++ newsfeed/src/app/feeds/allfeeds/allfeeds.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AllfeedsComponent } from './allfeeds.component';
+
+describe('AllfeedsComponent', () => {
+  let component: AllfeedsComponent;
+  let fixture: ComponentFixture<AllfeedsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AllfeedsComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(AllfeedsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
