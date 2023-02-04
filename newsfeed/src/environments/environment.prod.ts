59c85a0e0f5b1d28d99868ae80c89080dd6ef810
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2023-02-04 21:37:25 +0300

    add api to environments

--- newsfeed/src/environments/environment.prod.ts
+++ newsfeed/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://friendly-umbrella-cow.cyclic.app'
+
 };
--- newsfeed/src/environments/environment.ts
+++ newsfeed/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://friendly-umbrella-cow.cyclic.app',
+  randomUrl: 'https://randomuser.me/api/'
+  
+
 };
 
 /*
